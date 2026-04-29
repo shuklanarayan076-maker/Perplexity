8b67d86bf34724b2aa63129e5c6955446989ed73
Narayan prasad shukla <shuklanarayan@076gmail.com>
2026-04-29 17:47:40 +0530

    small fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -37,7 +37,7 @@ app.use("/api/chats",chatRouter)
 app.use(express.static(path.join(__dirname, "../dist")))
 
 
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
     res.sendFile(path.join(__dirname, "../dist", "index.html"))
 })
 
