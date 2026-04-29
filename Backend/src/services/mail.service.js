f6d05d33d448d791371c8b21ecaba63f4ee543af
Narayan prasad shukla <shuklanarayan@076gmail.com>
2026-04-29 18:59:37 +0530

    .env variables updated

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth:{
-        user: process.env.GOOGLE_USER,
-        pass: process.env.GOOGLE_PASSWORD
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
     }
 })
 
